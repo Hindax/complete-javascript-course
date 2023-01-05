0bb7b5827b2495162f54a39231b99f226264a50a
Hinda <hind.aarif@gmail.com>
2023-01-05 08:29:53 +0100

    Game Over Score

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -7,7 +7,8 @@ textContent);
 
 let secretNumber = Math.trunc(Math.random() * 20)+1;
 let score = 20;
-//document.querySelector('.number').textContent = secretNumber;
+let highscore =0;
+
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number (document.querySelector('.guess').value);
@@ -26,6 +27,11 @@ document.querySelector('.check').addEventListener('click', function(){
 
         document.querySelector('.number').style.width ='30rem';
 
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
 
 
     } else if (guess > secretNumber) {
@@ -38,11 +44,18 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.score').textContent = 0;
         }
     } else if (guess < secretNumber) {
+        if (score > 1) {
         document.querySelector('.message').textContent = 'Too low!';
         score--;
         document.querySelector('.score').textContent = score;
+
+    } else {
+        document.querySelector('.message').textContent = 'You lost the game';
+        document.querySelector('.score').textContent = 0;
     }
 
+
+}
 });
 
 document.querySelector('.again').addEventListener('click', function(){
@@ -59,19 +72,6 @@ document.querySelector('.again').addEventListener('click', function(){
 });
 
 
-// document.querySelector('.again').addEventListener('click', function () {
-//     score = 20;
-//     secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-//     document.querySelector('.message').textContent = 'Start guessing...';
-//     displayMessage('Start guessing...');
-//     document.querySelector('.score').textContent = score;
-//     document.querySelector('.number').textContent = '?';
-//     document.querySelector('.guess').value = '';
-
-//     document.querySelector('body').style.backgroundColor = '#222';
-//     document.querySelector('.number').style.width = '15rem';
-//   });
 
 
 
