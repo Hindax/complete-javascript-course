5422f69405198a3527d0e7921c76c02fb59c3a20
Hinda <hind.aarif@gmail.com>
2023-01-18 12:41:20 +0100

    Diplay starting tatus

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+// Selecting elements (buttons,value)
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling the dice: display dice, generate number, update current
+
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function(){
+    //generate a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //check for rolled 1: if true, switch to next player
+    if(dice !== 1) {
+        //Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; //change it depending in the palyer
+
+
+    } else {
+        // switch to next player
+        
+
+    }
+})
+
+
+
